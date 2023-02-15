27eab281fe3bb8a2cad0456eee0682717264aca8
Jef Myers <jefmyers@gmail.com>
2023-02-14 20:00:16 -0500

    Use Pydatnic

--- backend/engine.py
+++ backend/engine.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+
 from enum import Enum
 from random import Random
 from typing import Dict, List, ClassVar, Union
@@ -21,21 +22,27 @@ class MissingPlayerException(Exception):
     ...
 
 
-Suit = Enum(
-    "Suit",
-    """
-    Diamond Club
-    Heart Spade
-""",
-)
-Rank = Enum(
-    "Rank",
-    """
-    Two Three Four Five Six
-    Seven Eight Nine Ten
-    Jack Queen King Ace
-""",
-)
+class Suit(Enum):
+    Diamond = "Diamond"
+    Club = "Club"
+    Heart = "Heart"
+    Spade = "Spade"
+
+
+class Rank(Enum):
+    Two = "Two"
+    Three = "Three"
+    Four = "Four"
+    Five = "Five"
+    Six = "Six"
+    Seven = "Seven"
+    Eight = "Eight"
+    Nine = "Nine"
+    Ten = "Ten"
+    Jack = "Jack"
+    Queen = "Queen"
+    King = "King"
+    Ace = "Ace"
 
 
 class Card(BaseModel):
@@ -43,52 +50,53 @@ class Card(BaseModel):
     suit: Suit
 
     SUITS: ClassVar[dict] = {
-        Suit.Diamond: "\N{black diamond suit}",
-        Suit.Club: "\N{black club suit}",
-        Suit.Heart: "\N{black heart suit}",
-        Suit.Spade: "\N{black spade suit}",
+        "Diamond": "\N{black diamond suit}",
+        "Club": "\N{black club suit}",
+        "Heart": "\N{black heart suit}",
+        "Spade": "\N{black spade suit}",
     }
     RANKS: ClassVar[dict] = {
-        Rank.Two: " 2 ",
-        Rank.Three: " 3 ",
-        Rank.Four: " 4 ",
-        Rank.Five: " 5 ",
-        Rank.Six: " 6 ",
-        Rank.Seven: " 7 ",
-        Rank.Eight: " 8 ",
-        Rank.Nine: " 9 ",
-        Rank.Ten: " 10",
-        Rank.Jack: " J ",
-        Rank.Queen: " Q ",
-        Rank.King: " K ",
-        Rank.Ace: " A ",
+        "Two": " 2 ",
+        "Three": " 3 ",
+        "Four": " 4 ",
+        "Five": " 5 ",
+        "Six": " 6 ",
+        "Seven": " 7 ",
+        "Eight": " 8 ",
+        "Nine": " 9 ",
+        "Ten": " 10",
+        "Jack": " J ",
+        "Queen": " Q ",
+        "King": " K ",
+        "Ace": " A ",
     }
     VALUES: ClassVar[dict] = {
-        Rank.Ace: 1,
-        Rank.Two: 2,
-        Rank.Three: 3,
-        Rank.Four: 4,
-        Rank.Five: 5,
-        Rank.Six: 6,
-        Rank.Seven: 7,
-        Rank.Eight: 8,
-        Rank.Nine: 9,
-        Rank.Ten: 10,
-        Rank.Jack: 10,
-        Rank.Queen: 10,
-        Rank.King: 10,
+        "Ace": 1,
+        "Two": 2,
+        "Three": 3,
+        "Four": 4,
+        "Five": 5,
+        "Six": 6,
+        "Seven": 7,
+        "Eight": 8,
+        "Nine": 9,
+        "Ten": 10,
+        "Jack": 10,
+        "Queen": 10,
+        "King": 10,
     }
 
     class Config:
-        keep_untouched = (cached_property,)
+        # keep_untouched = (cached_property,)
+        use_enum_values = True
 
-    @cached_property
+    @property
     def value(self):
         return self.VALUES[self.rank]
 
-    @cached_property
+    @property
     def symbol(self):
-        if self.suit in {Suit.Heart, Suit.Diamond}:
+        if self.suit in {"Heart", "Diamond"}:
             return (
                 f"\033[1;41m{self.RANKS[self.rank]}{self.SUITS[self.suit]} \033[1;37;0m"
             )
@@ -111,19 +119,18 @@ class Player(BaseModel):
 
 
 class Unit(BaseModel):
-    cards: frozenset
+    cards: List[Card]
     value: int | None = None
 
     def __hash__(self):
-        return hash((self.cards, self.value))
+        return hash((tuple(self.cards), self.value))
 
     @classmethod
     def from_card(cls, card):
-        return cls(cards=frozenset(card), value=card.value)
+        return cls(cards=[card], value=card.value)
 
     def render(self):
         if len(self.cards):
-            # XXX Need nwise rank/suit
             return "【{}】".format(" ".join(c.symbol.lstrip() for c in self.cards))
         return " ".join(c.symbol for c in self.cards)
 
@@ -132,19 +139,17 @@ class Unit(BaseModel):
             raise ValueError("cannot combine")
         if (self.value + other.value) > max(Card.VALUES.values()):
             raise ValueError("cannot combine")
-        return Unit(
-            cards=frozenset({*self.cards, *other.cards}), value=self.value + other.value
-        )
+        return Unit(cards=[*self.cards, *other.cards], value=self.value + other.value)
 
 
 class State(BaseModel):
-    deck: deque[Card]
+    deck: list[Card]
     table: list[Unit]
     # XXX: Validate no more then six
     players: list[Player]
     player_order: List[Player]
-    hands: dict # Dict[Player, frozenset] | None = None
-    capture: Dict[Player, frozenset] | None = None
+    hands: Dict[str, List[Card]] 
+    capture: Dict[str, List[Card]] | None = None
 
     @contextmanager
     def players_turn(self, player_name):
@@ -161,108 +166,122 @@ class State(BaseModel):
         if _player != self.player_order[0]:
             raise NotTurnException(f"Not {player_name!r}'s turn")
 
-        player = self.player_order.popleft()
+        player = self.player_order.pop(0)
         try:
             yield player
         finally:
             self.player_order.append(player)
 
     @classmethod
-    def from_players(cls, deck, *players):
+    def from_players(cls, deck, players):
+        # XXX: We should fix how this deals and go 2 at a time to everyone.
 
-        _players = frozenset(players)
-        player_order = list(players)
+        player_order = players
 
         hands = defaultdict(list)
         for p in players:
             for _ in range(2):
                 card_one = deck.pop()
                 card_two = deck.pop()
-                hands[p].append(card_one)
-                hands[p].append(card_two)
-        
+                hands[p.name].append(card_one)
+                hands[p.name].append(card_two)
+
         table = [Unit.from_card(deck.pop()) for _ in range(4)]
-        hands = {pl: cards for pl, cards in hands.items()}
-        
+        hands = {pl_name: cards for pl_name, cards in hands.items()}
+
         return cls(
             deck=deck,
             table=table,
             players=players,
             hands=hands,
-            capture={pl: frozenset() for pl in players},
-            player_order=player_order
+            capture={pl.name: [] for pl in players},
+            player_order=player_order,
         )
 
     def with_discard(self, player, card_idx):
-        deck = deque([*self.deck])
-        table = [*self.table]
-        hands = {}
-        for pl, h in self.hands.items():
-            # Discard
-            if pl.name == player.name:
-                # Players hand
-                hand = [*self.hands[player]]
-                # The card to discard to the table
-                card = hand[card_idx]
-                # Remove from players hand
-                hand.remove(card)
-                # Add to the tables hand
-                print(card)
-                table.append(Unit.from_card(card))
-                # Deal the player another card automatically
-                hand.append(deck.pop())
-                # Replace the players hand with the new hand
-                hands[pl] = hand
-            else:
-                # Not the player so just keep everything the same
-                hands[pl] = h
+        with self.players_turn(player) as current_player:
+            deck = [*self.deck]
+            table = [*self.table]
+            hands = {}
+            for pl_name, h in self.hands.items():
+                # Discard
+                # XXX: Ugly string compareison... What if two players have the same name
+                if pl_name == current_player.name:
+                    # Players hand
+                    hand = [*self.hands[current_player.name]]
+                    # The card to discard to the table
+                    # Remove from players hand
+                    card = hand.pop(card_idx)
+                    # Add to the tables hand
+                    table.append(Unit.from_card(card))
+                    # Deal the player another card automatically
+                    hand.append(deck.pop())
+                    # Replace the players hand with the new hand
+                    hands[current_player.name] = hand
+                else:
+                    # Not the player so just keep everything the same
+                    hands[pl_name] = h
 
         # This introduces the df = df(...) idea. Should we write our own
         # replace? This feels kinda clumbsy.
         return State(
             deck=deck,
-            table=frozenset(table),
+            table=table,
             players=self.players,
             hands=hands,
             capture=self.capture,
             player_order=self.player_order,
         )
 
-    # def with_build(self, player, card, *targets):
-    #     deck = [*self.deck]
-    #     table = {*self.table}
-    #     hand = {*self.hands[player]}
-    #
-    #     hand.remove(card)
-    #     table.difference_update(targets)
-    #     table.add(reduce(or_, {*targets, Unit.from_card(card)}))
-    #     hand.add(deck.pop())
-    #
-    #     return replace(
-    #         self,
-    #         deck=deck,
-    #         table=frozenset(table),
-    #         hands={**self.hands, player: frozenset(hand)},
-    #     )
-    #
-    # def with_capture(self, player, card, *targets):
-    #     deck = [*self.deck]
-    #     table = {*self.table}
-    #     hand = {*self.hands[player]}
-    #
-    #     if not all(card.value == t.value for t in targets):
-    #         raise ValueError(f"cannot capture {targets} with {card}")
-    #
-    #     hand.remove(card)
-    #     table.difference_update(targets)
-    #     hand.add(deck.pop())
-    #
-    #     return replace(
-    #         self,
-    #         deck=deck,
-    #         table=frozenset(table),
-    #         hands={**self.hands, player: frozenset(hand)},
-    #     )
+    def with_build(self, player, card_idx, target_idx):
+        with self.players_turn(player) as current_player:
+            deck = self.deck
+            table = self.table
+            hand = self.hands[current_player.name]
+
+            card = hand.pop(card_idx)
+            # Add a card to the table unit
+            table[target_idx].cards.append(card)
+            hand.append(deck.pop())
+
+        return State(
+            deck=deck,
+            table=table,
+            players=self.players,
+            hands={**self.hands, current_player.name: hand},
+            capture=self.capture,
+            player_order=self.player_order,
+        )
+
+    def with_capture(self, player, card_idx, target_idx):
+        with self.players_turn(player) as current_player:
+            deck = self.deck
+            table = self.table
+            hand = self.hands[current_player.name]
+            capture = self.capture[current_player.name]
+
+            card = hand.pop(card_idx)
+
+            if not all(u.value == card.value for u in table[target_idx].cards):
+                raise ValueError(f"cannot capture {target_idx} with {card}")
+
+            # Capture was successful, remove the card and the table card
+            # XXX: Need to add this to capture
+            table_unit_captured = table.pop(target_idx)
+
+            # Not sure what capture should look like
+            capture.append(table_unit_captured)
+
+            hand.append(deck.pop())
+
+        return State(
+            deck=deck,
+            table=table,
+            players=self.players,
+            hands={**self.hands, current_player.name: hand},
+            capture={**self.capture, current_player.name: capture},
+            player_order=self.player_order,
+        )
 
     def render(self):
         # XXX: Make these line up! This is so annoying
@@ -272,10 +291,8 @@ class State(BaseModel):
                 f"Current Turn: {self.player_order[0].name}\n",
                 f"Table: {' '.join(u.render() for u in self.table)}\n",
                 *(
-                    f"\n{p.name:<8} {'  '.join(c.symbol for c in h)} \n"
-                    for p, h in sorted(
-                        self.hands.items(), key=lambda pl_h: pl_h[0].name
-                    )
+                    f"\n{p:<8} {'  '.join(c.symbol for c in h)} \n"
+                    for p, h in sorted(self.hands.items(), key=lambda pl_h: pl_h)
                 ),
             ]
         )
@@ -288,7 +305,7 @@ def game(players, seed=0, _deck=None):
     else:
         deck = [c for c in STANDARD_DECK]
         rnd.shuffle(deck)
-    state = State.from_players(deque(deck), *players)
+    state = State.from_players(deck, players)
 
     return state
 
@@ -303,17 +320,29 @@ if __name__ == "__main__":
     ]
     state = game(players)
 
-    print(f"{ state.dict()   = }")
+    serilaized = state.dict()
 
-    # from json import dump
-    # with open("state_dump.json", "wt") as fp:
-    #     dump(state.dict(), fp, indent=2)
-
+    state = State(**serilaized)
     print(state.render())
-
+    print(" ")
+    # print(state.render())
+    # state = state.with_discard("Hyacinth", 1)
     # print(" ")
-    # with state.players_turn("Hyacinth") as player:
-    #     state = state.with_discard(player, 1)
+    # # print(state.render())
+    # state = state.with_discard("Rose", 1)
+    # print(" ")
+    # # print(state.render())
+    # state = state.with_discard("Daisy", 1)
+    # print(" ")
+    # # print(state.render())
+    # state = state.with_capture("Onslow", 1, 2)
     #
+    # print(" ")
+    # # print(state.render())
+    # state = state.with_build("Hyacinth", 1, 0)
     #
     # print(state.render())
+    # from json import dump
+    #
+    # with open("state_dump.json", "wt") as fp:
+    #     dump(state.dict(), fp, indent=2)
